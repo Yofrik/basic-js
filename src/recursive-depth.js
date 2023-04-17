07d66f80ab09477cff3bf79082aa224f459dffad
Vladislav Tikhonovich <vt@rmz.by>
2023-04-17 22:20:12 +0300

    Completed more tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 0
+    for (let i in arr){
+      if(typeof arr[i] === 'object'){
+        count ++
+        count += this.calculateDepth(arr[i])
+      }
+    }
+    return count
   }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  let cur_date = new Date(date)
+  if(date != ''){
+    if(cur_date.getFullYear() > 0 && cur_date.getMonth() >= 0 && cur_date.getMonth() < 12 && cur_date.getDate() > 0 &&  cur_date.getDate() <= new Date(cur_date.getFullYear(), cur_date.getMonth() + 1, 0).getDate()){
+      if(cur_date.getMonth() == 11 || cur_date.getMonth() == 12 || cur_date.getMonth() == 1 || cur_date.getMonth() == 0){
+        return ('winter')
+      }
+      if(cur_date.getMonth() >= 2 && cur_date.getMonth() <= 4){
+        return ('spring')
+      }
+      if(cur_date.getMonth() >= 5 && cur_date.getMonth() <= 7){
+        return ('summer')
+      }
+      if(cur_date.getMonth() >= 8 && cur_date.getMonth() <= 10){
+        return ('autumn')
+      }
+    }
+    else return('Unable to determine the time of year!')
+  }
+  else {
+    return("Invalid date!")
+  }
+  
 }
 
 module.exports = {
